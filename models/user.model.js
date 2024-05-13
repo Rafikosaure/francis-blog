7ea2d3ec6750453cdd00a3ca4d9a735c6be66008
RAFIK BEN_SADI <RAFIKBENSADI@live.fr>
2024-05-13 13:02:49 +0200

    Reprise du modèle "User", cette fois-ci avec Sequelize (et non-plus Mongoose).

--- models/user.model.js
+++ models/user.model.js
@@ -1,15 +1,26 @@
-import mongoose from "mongoose";
-import mongooseUniqueValidator from "mongoose-unique-validator";
+export default ( Sequelize, DataTypes ) => {
 
-const userSchema = mongoose.Schema(
-  {
-    firstname: { type: String, require: true },
-    picture: { type: String, require: true },
-    email: { type: String, require: true, unique: true },
-    password: { type: String, require: true }
-  },
-  { timestamps: { createdAt: true } }
-)
-userSchema.plugin(mongooseUniqueValidator)
-
-export default mongoose.model('User', userSchema)
+    Sequelize.define(
+    'User',
+    {
+      firstname: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      lastname: {
+        type: DataTypes.STRING,
+        // allowNull est "true" par defaut
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false
+      }
+    },
+    { timestamps: true }
+  )
+}
